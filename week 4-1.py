3ecba2b93893a725d496d233f4733d268bc84de7
Brainrotz <158850397+Brainrotz@users.noreply.github.com>
2024-02-05 11:02:33 +0000

    Add files via upload

---
+++ week 4-1.py
@@ -0,0 +1,7 @@
+mylist = [10, 20, 30, 40]
+
+output = 1
+for num in mylist:
+    output *= num
+
+print(output)
---
+++ week 4-2.py
@@ -0,0 +1,5 @@
+wordList = ["comfortable", "round", "support", "machinery"]
+
+for word1 in wordList:
+    for word2 in wordList:
+        print(f"{word1} {word2}")
